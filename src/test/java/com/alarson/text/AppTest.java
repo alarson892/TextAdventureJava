fa4ff1aa4e87ce727e24014af3373a81480e1430
curt.larson <curt94@gmail.com>
2020-11-27 16:31:41 -0600

    Added maven pom.xml added simple helloworld class and unit test updated readme with steps to install pre-reqs

---
+++ src/test/java/com/alarson/text/AppTest.java
@@ -0,0 +1,20 @@
+package com.alarson.text;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
